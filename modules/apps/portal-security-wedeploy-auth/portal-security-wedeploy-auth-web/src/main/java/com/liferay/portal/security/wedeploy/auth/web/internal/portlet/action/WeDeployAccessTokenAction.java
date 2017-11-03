9dfd45468c8df45aef9318b4b52c4c1c8c3c5678
Jesse Rao <jesse.rao@liferay.com>
2017-11-03 13:11:30 -0400

    LPS-75566 WeDeploy Security

--- modules/apps/portal-security-wedeploy-auth/portal-security-wedeploy-auth-web/src/main/java/com/liferay/portal/security/wedeploy/auth/web/internal/portlet/action/WeDeployAccessTokenAction.java
+++ modules/apps/portal-security-wedeploy-auth/portal-security-wedeploy-auth-web/src/main/java/com/liferay/portal/security/wedeploy/auth/web/internal/portlet/action/WeDeployAccessTokenAction.java
@@ -44,7 +44,7 @@ import org.osgi.service.component.annotations.Reference;
  * @author Supritha Sundaram
  */
 @Component(
-	immediate = true, property = {"path=/portal/wedeploy/access_token"},
+	property = {"path=/portal/wedeploy/access_token"},
 	service = StrutsAction.class
 )
 public class WeDeployAccessTokenAction extends BaseStrutsAction {
